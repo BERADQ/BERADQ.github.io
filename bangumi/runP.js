a0f3684bf98484588eb68acccda48726e9e6cb53
Observer <78293733+BERADQ@users.noreply.github.com>
2022-12-27 13:57:05 +0800

    Add files via upload

---
+++ bangumi/runP.js
@@ -0,0 +1,37 @@
+let pAr;
+if (!(typeof (playarr) == "undefined" || playarr == null)) {
+	pAr = playarr;
+	console.log(pAr);
+	runPlayer(pAr,'playarr');
+}
+if (!(typeof (playarr_lz) == "undefined" || playarr_lz == null)) {
+	pAr = playarr_lz;
+	console.log(pAr);
+	runPlayer(pAr,'playarr_lz');
+}
+if (!(typeof (playarr_wj) == "undefined" || playarr_wj == null)) {
+	pAr = playarr_wj;
+	console.log(pAr);
+	runPlayer(pAr,'playarr_wj');
+}
+if (!(typeof (playarr_1) == "undefined" || playarr_1 == null)) {
+	pAr = playarr_1;
+	console.log(pAr);
+	runPlayer(pAr,'playarr_1');
+}
+if (!(typeof (playarr_2) == "undefined" || playarr_2 == null)) {
+	pAr = playarr_2;
+	console.log(pAr);
+	runPlayer(pAr,'playarr_2');
+}
+if (!(typeof (playarr_2) == "undefined" || playarr_2 == null)) {
+	pAr = playarr_2;
+	console.log(pAr);
+	runPlayer(pAr,'playarr_2');
+}
+if (!(typeof (playarr_fs) == "undefined" || playarr_fs == null)) {
+	pAr = playarr_fs;
+	console.log(pAr);
+	runPlayer(pAr,'playarr_fs');
+}
+
