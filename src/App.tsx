3695aacb92e3d687be78aaac6136b9f7d128c41c
BERADQ <d1921576220@163.com>
2023-05-31 20:37:54 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ import { NavItem } from "./nav/types";
 const App: Component = () => {
   if (!window.location.hash) window.location.hash = localStorage.getItem("ninnana.top.log.pagehash") || "#home";
   const pags: NavItem[] = [
-    { to: "#home", target: "_blank", name: "首页" },
-    { to: "#doc", target: "_blank", name: "文档" },
+    { to: "#home", target: "_blank", name: "首   页" },
+    { to: "#plugin-list", target: "_blank", name: "插件列表" },
+    { to: "#doc", target: "_blank", name: "文   档" },
+    { to: "#chat", target: "_blank", name: "AI 聊天" },
   ];
   const [selectedTab, setSelectedTab] = createSignal(
     localStorage.getItem("ninnana.top.log.pagehash") || "#home"
